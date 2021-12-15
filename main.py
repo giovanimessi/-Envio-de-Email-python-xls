db4731b7933f117ffd2c328faab2c43cccb7c3c1
Giovani <giovanimouradev@gmail.com>
2021-12-15 00:17:08 -0300

    envio de email usado arquivo xlsx

--- main.py
+++ main.py
@@ -1,56 +1,33 @@
-#importa a base de dados
-#visualizar base de dados
-
-#pip install  pywin32
-#envio de email
-
-#utlizado panda biblioteca #instalar panda->pip install pandas e open pip install openpyxl
-
-#faturamento por loja
-
-#quantidade de produtos vendidos por loja
-
-#tcket medio por produto em cada loja(faturamento dividido por quantidade de produto vendido por loja
-
-#Envio do email com relatorio
-
-#filtrar por coluna = tabela_venda[['ID Loja','Valor Final']]
-#tabela_venda.grouby('ID Loja').sum()
-#tabelas_vendas[['ID Loja','Valor Final']].grouby('ID Loja').sum()
-
+import smtplib
+import ssl
+import email.message
 
 import pandas as pd
-#ler o arquivo xml
+
 import win32com.client as win32
 
+# importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
 
-tabela_vendas = pd.read_excel('vendas.xlsx')
-
-
+# visualizar a base de dados
 pd.set_option('display.max_columns', None)
+print(tabela_vendas)
 
-faturamento = tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum()
-#quantidade de produtos vendidos por loja
+# faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
-print('-' * 50)
-quantidade = tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum()
 
-
+# quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
 print(quantidade)
 
 print('-' * 50)
-#ticket medio
-ticket =  (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+# ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
+print(ticket_medio)
 
-print(ticket)
-
-
-# enviar um email com o relatório
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
-mail.To = 'pythonimpressionador@gmail.com'
-mail.Subject = 'Relatório de Vendas por Loja'
-mail.HTMLBody = f'''
+body = f"""
 <p>Prezados,</p>
 
 <p>Segue o Relatório de Vendas por cada Loja.</p>
@@ -62,23 +39,32 @@ mail.HTMLBody = f'''
 {quantidade.to_html()}
 
 <p>Ticket Médio dos Produtos em cada Loja:</p>
-{ticket.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
 
 <p>Qualquer dúvida estou à disposição.</p>
 
 <p>Att.,</p>
 <p>Lira</p>
-'''
 
-mail.Send()
+"""
+msg = email.message.Message()
+msg['Subject'] = "Relatorio de vendas "
 
-print('Email Enviado')
+msg ['From'] = 'pythomemail@gmail.com'
+msg['To'] = "pythomemail@gmail.com"
+password = "Magneto2020"
+msg.add_header('Content-type','text/html')
+msg.set_payload(body)
 
 
+context = ssl.create_default_context()
+with smtplib.SMTP('smtp.gmail.com',587) as conexao:
+    conexao.ehlo()
+    conexao.starttls(context=context)
+    conexao.login(msg['From'],password)
 
-
-
-
+    conexao.sendmail(msg['From'],msg['To'],msg.as_string().encode('utf-8'))
+    print("Email enviado")
 
 
 
